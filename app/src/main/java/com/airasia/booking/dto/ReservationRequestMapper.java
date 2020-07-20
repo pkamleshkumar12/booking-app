b471742a8cc8de6426aad43a8b2ea1416ec642c7
kamlesh <p.kamlesh@touchngo.com.my>
2020-07-21 02:41:49 +0800

    minor fixes missing fields in ReservationResponse

--- app/src/main/java/com/airasia/booking/dto/ReservationRequestMapper.java
+++ app/src/main/java/com/airasia/booking/dto/ReservationRequestMapper.java
@@ -1,6 +1,8 @@
 package com.airasia.booking.dto;
 
+import com.airasia.booking.entities.Customer;
 import com.airasia.booking.entities.Reservation;
+import com.airasia.booking.model.CustomerDetails;
 import com.airasia.booking.model.ReservationRequestResponse;
 import lombok.val;
 import org.modelmapper.ModelMapper;
@@ -13,6 +15,9 @@ public class ReservationRequestMapper {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private RoomRequestMapper roomRequestMapper;
+
     public ReservationRequestMapper(ModelMapper modelMapper){
         this.modelMapper = modelMapper;
         this.modelMapper.createTypeMap(ReservationRequestResponse.class, Reservation.class)
@@ -22,8 +27,19 @@ public class ReservationRequestMapper {
 
     public ReservationRequestResponse convertToDto(Reservation reservation){
 
+        val customerDetails = new CustomerDetails()
+                .email(reservation.getCustomer().getCustomerEmail())
+                .name(reservation.getCustomer().getCustomerFullName())
+                .phoneNumber(reservation.getCustomer().getCustomerPhoneNumber());
+
+        val room =roomRequestMapper.convertToDto(reservation.getRoom());
 
         val dto = modelMapper.map(reservation, ReservationRequestResponse.class);
+        dto.setHotelId(reservation.getHotel().getId());
+        dto.setHotelName(reservation.getHotel().getName());
+        dto.setCustomer(customerDetails);
+        dto.setRoom(room);
+
         return dto;
     }
 }
